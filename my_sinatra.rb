8f6fa1d2bc1f3b04594793f669832444e2eeddb0
Ekaterina12345 <iiiii21111998@gmail.com>
2021-11-25 21:27:33 +0300

    feat: add sinatra

--- my_sinatra.rb
+++ my_sinatra.rb
@@ -1,16 +1,19 @@
 # запросы API
 
 require 'sinatra'
+require 'sinatra/namespace'
 
 get '/' do
   'Hello My Sinatra - Easy and Wide world!'
 end
 
 
-get ‘hello/:name’ do
-  "Sinatra приветствует тебя, #{params[:name]}!"
+get '/hello/:name' do
+  "Sinatra приветствует тебя, #{params['name']}!"
 end
 
-get ‘/*’ do
-  "I don't know what is the #{params[:splat]}. It's what you typed."
+namespace '/api/v1' do
+  get '/*' do
+    "I don't know what is the #{params['splat']}. It's what you typed."
+    end
 end
